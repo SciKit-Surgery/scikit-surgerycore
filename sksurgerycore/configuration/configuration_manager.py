4451990d1d08c964517d07042af20f03ac054531
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-11-21 17:18:35 +0000

    Issue #3: New module for ConfigurationManager class.

---
+++ sksurgerycore/configuration/configuration_manager.py
@@ -0,0 +1,16 @@
+#  -*- coding: utf-8 -*-
+
+"""Class to load application configuration information from file."""
+
+
+class ConfigurationManager:
+    """
+    Class to load application configurationm for example
+    at startup of an application.
+
+    """
+    def __init__(self):
+        """
+        Constructor.
+        """
+        pass
---
+++ tests/configuration/test_configuration_manager.py
@@ -0,0 +1,8 @@
+#  -*- coding: utf-8 -*-
+
+import pytest
+import sksurgerycore.configuration.configuration_manager as m
+
+
+def test_constructor():
+    pass
