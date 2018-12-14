feb036922b9539892339040cf26674e70bf7fe05
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-12-14 08:31:36 +0000

    Issue #8: Fix validate_is_file error messages, improve unit tests.

--- tests/utilities/test_validate_file.py
+++ tests/utilities/test_validate_file.py
@@ -8,13 +8,31 @@ import pytest
 from sksurgerycore.utilities import validate_file as f
 
 
+def test_invalid_file_name_because_its_null():
+
+    with pytest.raises(ValueError):
+        f.validate_is_file(None)
+
+
+def test_invalid_file_name_because_its_empty():
+
+    with pytest.raises(ValueError):
+        f.validate_is_file("")
+
+
+def test_invalid_file_name_because_its_an_integer():
+
+    with pytest.raises(TypeError):
+        f.validate_is_file(1)
+
+
 def test_invalid_file_name_because_non_existent():
 
     with pytest.raises(ValueError):
         f.validate_is_file("/invalid/file/name")
 
 
-def test_invalid_file_name_because_directory():
+def test_invalid_file_name_because_its_a_directory():
 
     with pytest.raises(ValueError):
         f.validate_is_file(".")
