11903a60ca946c2e6cfe8a08b365276afeb083a4
rmapaah <rmapaah@ucl.ac.uk>
2019-02-26 22:42:13 +0000

    Issue #13: Unit test added
    Unit test added to check the system exit case when range is less than 6

--- tests/algorithms/test_pivot.py
+++ tests/algorithms/test_pivot.py
@@ -12,6 +12,16 @@ def test_empty_matrices4x4():
         p.pivot_calibration(None)
 
 
+def test_range_le_six():
+    with pytest.raises(SystemExit):
+        file_names = glob('tests/algorithms/PivotCalibration/1378476416922755200.txt')
+        arrays = [np.loadtxt(f) for f in file_names]
+        matrices = np.concatenate(arrays)
+        numberOf4x4Matrices = int(matrices.size/16)
+        matrices4x4 = matrices.reshape(numberOf4x4Matrices, 4, 4)
+        p.pivot_calibration(matrices4x4)
+
+
 def test_four_columns_matrices4x4():
     with pytest.raises(ValueError):
         p.pivot_calibration(np.arange(2, 11, dtype=float).reshape(3, 3))
@@ -29,5 +39,7 @@ def test_return_value():
     numberOf4x4Matrices = int(matrices.size/16)
     matrices4x4 = matrices.reshape(numberOf4x4Matrices, 4, 4)
     p.pivot_calibration(matrices4x4)
+
+
 
 
