31fb166bf5443c9bfd383d600a7bb66c176d3a31
rmapaah <rmapaah@ucl.ac.uk>
2019-02-27 15:32:48 +0000

    Issue #13: test code added
    Test code added to verify the dimensions of matrices after creation and after modification.

--- tests/algorithms/test_pivot.py
+++ tests/algorithms/test_pivot.py
@@ -8,11 +8,13 @@ from glob import glob
 
 
 def test_empty_matrices4x4():
+
     with pytest.raises(TypeError):
         p.pivot_calibration(None)
 
 
 def test_range_le_six():
+
     with pytest.raises(SystemExit):
         file_names = glob('tests/algorithms/PivotCalibration/1378476416922755200.txt')
         arrays = [np.loadtxt(f) for f in file_names]
@@ -23,23 +25,22 @@ def test_range_le_six():
 
 
 def test_four_columns_matrices4x4():
+
     with pytest.raises(ValueError):
         p.pivot_calibration(np.arange(2, 11, dtype=float).reshape(3, 3))
 
 
 def test_four_rows_matrices4x4():
+
     with pytest.raises(ValueError):
         p.pivot_calibration(np.arange(2, 11, dtype=float).reshape(3, 3))
 
 
 def test_return_value():
+
     file_names = glob('tests/algorithms/PivotCalibration/*')
     arrays = [np.loadtxt(f) for f in file_names]
     matrices = np.concatenate(arrays)
     numberOf4x4Matrices = int(matrices.size/16)
     matrices4x4 = matrices.reshape(numberOf4x4Matrices, 4, 4)
     assert 1.8385 == round(p.pivot_calibration(matrices4x4), 4)
-
-
-
-
