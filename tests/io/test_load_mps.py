cb9eb44023d0a74a540fbbabdb32f5e3c0624674
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-02-11 12:25:48 +0000

    Issue #24: Add basic .mps reader.

---
+++ tests/io/test_load_mps.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+"""Tests for load_mps"""
+
+import six
+import pytest
+import sksurgerycore.io.load_mps as lmps
+
+
+def test_load_mps():
+    ids, points = lmps.load_mps('tests/data/pointset.mps')
+    assert points.shape[0] == 3
+    assert points.shape[1] == 3
+    assert ids.shape[0] == 3
+
+
