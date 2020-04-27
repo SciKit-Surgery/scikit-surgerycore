2daf9a5c493c878166aa02bb797bb9383b34605c
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-04-27 11:10:18 +0100

    Issue #43 changed version number, to reflect change in API

--- sksurgerycore/algorithms/pivot.py
+++ sksurgerycore/algorithms/pivot.py
@@ -5,7 +5,7 @@
 def pivot_calibration(tracking_matrices):
     """
     sksurgerycore.algorithms.pivot_calibration is
-    depreciated from v0.5.2, please use
+    depreciated from v0.6.0, please use
     skscurgerycalibration.algorithms.pivot_calibration from
     from scikit-surgerycalibration instead
     :raises: NotImplementedError
@@ -13,7 +13,7 @@ def pivot_calibration(tracking_matrices):
 
     raise NotImplementedError(
         "sksurgerycore.algorithms.pivot_calibration is " +
-        "depreciated from v0.5.2, please use " +
+        "depreciated from v0.6.0, please use " +
         "skscurgerycalibration.algorithms.pivot_calibration from " +
         "from scikit-surgerycalibration instead")
 
@@ -25,7 +25,7 @@ def pivot_calibration_with_ransac(tracking_matrices,
                                   ):
     """
     sksurgerycore.algorithms.pivot_calibration_with_ransac is
-    depreciated from v0.5.2, please use
+    depreciated from v0.6.0, please use
     skscurgerycalibration.algorithms.pivot_calibration_with_ransac from
     from scikit-surgerycalibration instead
     :raises: NotImplementedError
@@ -33,6 +33,6 @@ def pivot_calibration_with_ransac(tracking_matrices,
 
     raise NotImplementedError(
         "sksurgerycore.algorithms.pivot_calibration_with_ransac is " +
-        "depreciated from v0.5.2, please use " +
+        "depreciated from v0.6.0, please use " +
         "skscurgerycalibration.algorithms.pivot_calibration_with_ransac " +
         "from scikit-surgerycalibration instead")
