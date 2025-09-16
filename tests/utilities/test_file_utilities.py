7df09708e4537ec95a1ef64d189b23a9cff09d85
Matt Clarkson <m.clarkson@ucl.ac.uk>
2025-09-16 08:12:31 +0100

    Issue #78: Update get_absolute_path_of_file, more checking, then rely more on os.path.abspath.

--- tests/utilities/test_file_utilities.py
+++ tests/utilities/test_file_utilities.py
@@ -7,6 +7,26 @@ import pytest
 
 import sksurgerycore.utilities.file_utilities as fu
 
+def test_get_absolute_file_exceptions():
+    with pytest.raises(ValueError) as excinfo:
+        fu.get_absolute_path_of_file(None)
+    assert "file_name is None." in str(excinfo.value)
+
+    with pytest.raises(TypeError) as excinfo:
+        fu.get_absolute_path_of_file(123)
+    assert "file_name is not a string." in str(excinfo.value)
+
+    with pytest.raises(ValueError) as excinfo:
+        fu.get_absolute_path_of_file("   ")
+    assert "file_name is empty." in str(excinfo.value)
+
+    with pytest.raises(TypeError) as excinfo:
+        fu.get_absolute_path_of_file("file.txt", dir_name=123)
+    assert "dir_name is not a string." in str(excinfo.value)
+
+    with pytest.raises(ValueError) as excinfo:
+        fu.get_absolute_path_of_file("file.txt", dir_name="   ")
+    assert "dir_name is empty." in str(excinfo.value)
 
 def test_get_absolute_file():
 
@@ -21,3 +41,11 @@ def test_get_absolute_file():
 
     fp3 = fu.get_absolute_path_of_file(this_file, cwd)
     assert fp3 == fp
+
+    # Similar to test for fp, but file doesn't exist.
+    # So, really just checking that we always get an absolute path.
+    fp4 = fu.get_absolute_path_of_file("nonsense.txt")
+    assert fp4 is not None
+    assert os.path.basename(fp4) == "nonsense.txt"
+    assert os.path.isabs(fp4)
+    assert os.path.dirname(fp4) == cwd
